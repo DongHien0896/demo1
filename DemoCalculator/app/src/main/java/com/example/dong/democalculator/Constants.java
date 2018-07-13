5ceb8c078170adb7af5c43a357be700965532ff3
DongHien0896 <donghien0896@gmail.com>
2018-07-13 09:06:00 +0700

    complete

---
+++ DemoCalculator/app/src/main/java/com/example/dong/democalculator/Constants.java
@@ -0,0 +1,31 @@
+package com.example.dong.democalculator;
+
+public class Constants {
+    public static final String EMPTY = " ";
+    public static final String NONE = "";
+    public static final char OPEN = '(';
+    public static final char CLOSE = ')';
+    public static final String TAX_DELETE = "\\s+";
+    public static final String ZERO = "0";
+    public static final String ONE = "1";
+    public static final String TWO = "2";
+    public static final String THREE = "3";
+    public static final String FOUR = "4";
+    public static final String FIVE = "5";
+    public static final String SIX = "6";
+    public static final String SEVEN = "7";
+    public static final String EIGHT = "8";
+    public static final String NINE = "9";
+    public static final String ADD = "+";
+    public static final char CHAR_ADD = '+';
+    public static final String SUB = "-";
+    public static final char CHAR_SUB = '-';
+    public static final String AC = "AC";
+    public static final String MUL = "x";
+    public static final char CHAR_MUL = 'x';
+    public static final String DIV = ":";
+    public static final char CHAR_DIV = ':';
+    public static final String MESSAGE_CLREA = "clear clicked!";
+    public static final String MESSAGE_SAVE = "save clicked!";
+    public static final int DEFAULT = 0;
+}
